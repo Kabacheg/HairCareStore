75bcfb385c0f824811e2be0b63b13bc82c0e2053
Kabacheg <mika.huseyn.chingiz@gmail.com>
2025-02-07 12:53:23 +0400

    Add option to work with SQL and JSON

---
+++ Models/ValidationResponse.cs
@@ -0,0 +1,12 @@
+namespace Hair_Care_Store.Models;
+
+public class ValidationResponse
+{
+    public string Property { get; set; }
+    public string Message { get; set; }
+    public ValidationResponse(string property, string message)
+    {
+        this.Property = property;
+        this.Message = message;
+    }
+}
